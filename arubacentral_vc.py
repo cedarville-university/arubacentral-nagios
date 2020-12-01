69e897037e96917caa2e7f3f84c1c14487557a31
Nat Biggs <nbiggs112@cedarville.edu>
2020-12-01 12:19:23 -0500

    makes args for vc script

--- arubacentral_vc.py
+++ arubacentral_vc.py
@@ -3,13 +3,25 @@
 from libarubacentral import ArubaCentralConfig, ArubaCentralAuth
 import os
 import time
+import argparse
 
-DEBUG = True
-GROUP = None
+tool_description = "This tool is used by collectd to get client count statistics per VC from Aruba Central"
+parser = argparse.ArgumentParser(description=tool_description, add_help=True)
+parser.add_argument("-g", "--group", help ="get clients from this group")
+parser.add_argument("-D", "--DEBUG", help ="turn on debug mode", action="store_true")
+args = parser.parse_args()
+
+group = None
+DEBUG = None
+
+if args.group:
+    group = args.group
+if args.DEBUG:
+    DEBUG = True
 
 session = ArubaCentralAuth(ArubaCentralConfig('Default', './config').read_config())
 
-vcs = session.get_vcs(GROUP)
+vcs = session.get_vcs(group=group)
 HOSTNAME = os.environ.get("COLLECTD_HOSTNAME")
 INTERVAL = os.environ.get("COLLECTD_INTERVAL")
 
